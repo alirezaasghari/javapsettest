5d027c4aacd16a40181bc9cb272819eef2a4e21b
Alireza <alireza@Alirezas-MacBook-Pro.local>
2018-03-21 19:53:19 +0100

    Add project structure

---
+++ src/main/java/at/refugeescode/javapsettest/JavapsettestApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.javapsettest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavapsettestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavapsettestApplication.class, args);
+	}
+}
